4d9c7ec97c43d8441f7790a646ec669eaf5711cf
Vishal Oza <Vishal.Oza@amsusa1.onmicrosoft.com>
2018-09-25 10:49:36 -0500

    unit test for Gtest

--- ledgerTest/test.cpp
+++ ledgerTest/test.cpp
@@ -1,7 +1,24 @@
 #include "pch.h"
 #include <data_ledger.h>
+#include <thread>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std::chrono_literals;
+
+TEST(TestCaseName, TestName) 
+{
+	data_ledger<int> leg(0);
+	EXPECT_EQ(1, 1);
+	leg.update(2);
+	std::this_thread::sleep_for(1ms);
+	leg.update(12);
+	std::this_thread::sleep_for(1ms);
+	leg.update(-4);
+	std::this_thread::sleep_for(1ms);
+	leg.update(3);
+	std::this_thread::sleep_for(1ms);
+	leg.update(7);
+	std::this_thread::sleep_for(1ms);
+	leg.update(4);
+	std::this_thread::sleep_for(1ms);
+	EXPECT_TRUE(true);
 }
